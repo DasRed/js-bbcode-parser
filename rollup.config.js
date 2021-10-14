c492a42dbcba1b2fe7ecc29dce8aa64c4166f6e0
Marco Starker <marco.starker@gmx.net>
2021-10-14 10:04:29 +0200

    dropping legacy support

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import {terser} from "rollup-plugin-terser";
 import clear from "rollup-plugin-clear";
-
-const babelOptionsES5 = {
-    runtimeHelpers:  false,
-    externalHelpers: false,
-    babelrc:         false,
-    presets:         [
-        [
-            "@babel/env",
-            {
-                targets: {
-                    ie:      '11',
-                    edge:    '17',
-                    firefox: '60',
-                    chrome:  '71',
-                    safari:  '11.1',
-                },
-            }
-        ]
-    ],
-    plugins:         [
-        ["@babel/plugin-proposal-class-properties", {loose: true}],
-        ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
-        ["@babel/plugin-proposal-private-methods", { loose: true }],
-    ]
-};
+import {terser} from "rollup-plugin-terser";
 
 const babelOptionsES2018 = {
     runtimeHelpers:  false,
@@ -35,110 +10,47 @@ const babelOptionsES2018 = {
     plugins:         [
         ["@babel/plugin-proposal-class-properties", {loose: true}],
         ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
-        ["@babel/plugin-proposal-private-methods", { loose: true }],
+        ["@babel/plugin-proposal-private-methods", {loose: true}],
     ]
 };
 
 // https://rollupjs.org/guide/en#big-list-of-options
 export default [
-    // ES2015 Minified
+    // ES Modules Minified
     {
         input:   './src/index.js',
         output:  {
             file:      './dist/parser.min.js',
-            format:    'umd',
-            name:      'BBCode',
+            format:    'esm',
             compact:   true,
             sourcemap: true,
         },
         plugins: [
-            resolve(),
             clear({targets: ['./dist']}),
-            babel(babelOptionsES5),
+            resolve(),
+            babel(babelOptionsES2018),
             terser(),
         ]
     },
-    // ES2015 None-Minified
+    // ES Modules None-Minified
     {
         input:   './src/index.js',
         output:  {
             file:      './dist/parser.js',
-            format:    'umd',
-            name:      'BBCode',
+            format:    'esm',
             compact:   false,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            babel(babelOptionsES5),
+            babel(babelOptionsES2018),
         ]
     },
     // ES Modules Minified
-    {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/parser.esm.min.js',
-            format:    'esm',
-            compact:   true,
-            sourcemap: true,
-        },
-        plugins: [
-            resolve(),
-            babel(babelOptionsES2018),
-            terser(),
-        ]
-    },
-    // ES Modules None-Minified
-    {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/parser.esm.js',
-            format:    'esm',
-            compact:   false,
-            sourcemap: true,
-        },
-        plugins: [
-            resolve(),
-            babel(babelOptionsES2018),
-        ]
-    },
-    // ES2015 Minified
     {
         input:   './src/simple.js',
         output:  {
             file:      './dist/simple.min.js',
-            format:    'umd',
-            name:      'BBCode',
-            compact:   true,
-            sourcemap: true,
-        },
-        plugins: [
-            resolve(),
-            clear({targets: ['./dist']}),
-            babel(babelOptionsES5),
-            terser(),
-        ]
-    },
-    // ES2015 None-Minified
-    {
-        input:   './src/simple.js',
-        output:  {
-            file:      './dist/simple.js',
-            format:    'umd',
-            name:      'BBCode',
-            compact:   false,
-            sourcemap: true,
-        },
-        plugins: [
-            resolve(),
-            babel(babelOptionsES5),
-        ]
-    },
-    // ES Modules Minified
-    {
-        input:   './src/simple.js',
-        output:  {
-            file:      './dist/simple.esm.min.js',
             format:    'esm',
             compact:   true,
             sourcemap: true,
@@ -153,7 +65,7 @@ export default [
     {
         input:   './src/simple.js',
         output:  {
-            file:      './dist/simple.esm.js',
+            file:      './dist/simple.js',
             format:    'esm',
             compact:   false,
             sourcemap: true,
